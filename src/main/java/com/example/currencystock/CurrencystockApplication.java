cdfdb6beee197670a43770ea532fef731eee821c
topKim <bighit.top@gmail.com>
2022-09-28 00:34:14 +0900

    init project

---
+++ src/main/java/com/example/currencystock/CurrencystockApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencystockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencystockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencystock/CurrencystockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
